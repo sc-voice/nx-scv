5811b87f5192ae90ae3f672af1c4f3ca693e5369
Karl Lew <karl@firepick.org>
2026-04-22 20:00:34 -0700

    v0.0.0 += @types/node@24

--- pkg/tools/src/text/color-console.ts
+++ pkg/tools/src/text/color-console.ts
@@ -243,18 +243,17 @@ export class ColorConsole {
       let oldStyles = Object.assign({}, styles);
       let oldColors = defaultOptions.colors;
       defaultOptions.colors = true;
-      const colorFunc = (str: string) => this.valueColor + str + NO_COLOR;
-      const underlineFunc = (str: string) => '\u001b[4m' + str + '\u001b[24m';
-      styles.bigint = colorFunc;
-      styles.boolean = colorFunc;
-      styles.date = colorFunc;
+      let valueColor = ColorConsole.utilColor(this.valueColor);
+      styles.bigint = valueColor;
+      styles.boolean = valueColor;
+      styles.date = valueColor;
       //styles.name = textColor;
-      styles.module = underlineFunc;
-      styles.null = colorFunc;
-      styles.number = colorFunc;
-      (styles.regexp as any) = colorFunc;
-      styles.special = colorFunc;
-      (styles.string as any) = colorFunc;
+      styles.module = 'underline';
+      styles.null = valueColor;
+      styles.number = valueColor;
+      styles.regexp = valueColor;
+      styles.special = valueColor;
+      styles.string = valueColor;
       //styles.undefined = valueColor;
 
       this.write(...this.color(color, ...rest));
